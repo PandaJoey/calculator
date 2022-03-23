91e2afa18794791ba96571d0d55ea683836e4af6
PandaJoey <pandajoey77@gmail.com>
2022-03-23 13:41:46 +0000

    fixed gradle files

---
+++ .history/src/test/java/com/leszko/calculator/CalculatorTest_20220323133743.java
@@ -0,0 +1,12 @@
+package com.leszko.calculator;
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testSum() {
+        assertEquals(5, calculator.sum(2, 3));
+    }
+}
